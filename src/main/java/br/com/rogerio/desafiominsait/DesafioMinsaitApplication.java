ed1cf5802e2d7062fd3f63645920c97f5ecafd4e
= <=>
2023-10-26 09:51:19 -0300

    Projeto criado

---
+++ src/main/java/br/com/rogerio/desafiominsait/DesafioMinsaitApplication.java
@@ -0,0 +1,13 @@
+package br.com.rogerio.desafiominsait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMinsaitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMinsaitApplication.class, args);
+	}
+
+}
